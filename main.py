1d496439f18f99bc62780a6a9987ada81e90ce67
Your Name <you@example.com>
2023-08-19 17:23:05 +0530

    Code to load data from .WAD file. Added code to draw Pygame window

---
+++ main.py
@@ -0,0 +1,43 @@
+from wad_data import WADData
+from settings import *
+import pygame as pg
+import sys
+
+
+class DoomEngine:
+    def __init__(self, wad_path='wad/DOOM1.WAD'):
+        self.wad_data = None
+        self.wad_path = wad_path
+        self.screen = pg.display.set_mode(WIN_RES)
+        self.clock = pg.time.Clock()
+        self.running = True
+        self.dt = 1 / 60
+        self.on_init()
+
+    def on_init(self):
+        self.wad_data = WADData(self, map_name='E1M1')
+
+    def update(self):
+        self.dt = self.clock.tick()
+        pg.display.flip()
+        pg.display.set_caption(f'{self.clock.get_fps()}')
+
+    def draw(self):
+        self.screen.fill('black')
+
+    def check_events(self):
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                self.running = False
+
+    def run(self):
+        while self.running:
+            self.check_events()
+            self.update()
+            self.draw()
+        pg.quit()
+
+
+if __name__ == "__main__":
+    doom = DoomEngine()
+    doom.run()
